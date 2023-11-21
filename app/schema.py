49746966a215bf9ea171335ec412664f53742476
dsdbagui <134153437+dsdbagui@users.noreply.github.com>
2023-11-21 17:34:48 -0300

    Revert "adicionando pydantic"

--- app/schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, PositiveInt
-from typing import Optional
-
-class ProdutosSchema(BaseModel):
-    id: int
-    nome: str
-    descricao: Optional[str]=None
-    preco: PositiveInt
---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from typing import List, Dict
+
+app = FastAPI()
+
+produtos: List[Dict[str, any]] = [
+    {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um smartphone top de linha",
+        "preco": 1500.00
+    },
+    {
+        "id": 2,
+        "nome": "Notebook",
+        "descricao": "Um notebook gamer",
+        "preco": 3500.00
+    },
+    {
+        "id": 3,
+        "nome": "Smart TV",
+        "descricao": "Uma smart TV 4K",
+        "preco": 2500.00
+    }
+]
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
+
+@app.get("/") #request
+def ola_mundo(): #response
+    return {"Olá": "Mundo"}
+
+@app.get("/produtos/{id}")
+def buscar_produto(id: int):
+    for produto in produtos:
+        if produto["id"] == id:
+            return produto
+    return {"Status": 404, "Mensagem": "Produto não encontrado"}
+
